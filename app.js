9ca87dd090ca02e9f9106f5dd1cc199343f116c9
Pedro Chavez Nolasco <pedro.chn01@gmail.com>
2022-05-06 12:05:40 -0700

    grabbed all dom elements

--- app.js
+++ app.js
@@ -2,12 +2,22 @@
 const ball1 = document.getElementById('ball1');
 const ball2 = document.getElementById('ball2');
 const ball3 = document.getElementById('ball3');
+const button1 = document.getElementById('shell1button');
+const button2 = document.getElementById('shell2button');
+const button3 = document.getElementById('shell3button');
+const winsSpan = document.getElementById('wins');
+const lossesSpan = document.getElementById('losses');
+const totalSpan = document.getElementById('total');
 
 
 // let state
 let wins = 0;
+let total = 0;
 
 // set event listeners 
+button1.addEventListener('click', () => {
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
+  
+});
