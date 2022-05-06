c00275bfa9b5e98feeb95da8d85962fbde35616b
Pedro Chavez Nolasco <pedro.chn01@gmail.com>
2022-05-06 12:22:24 -0700

    adding if else statement

--- app.js
+++ app.js
@@ -15,17 +15,30 @@ let wins = 0;
 let total = 0;
 
 function resetBalls() {
-  ball1.classList.remove('reveal'):
-  ball2.classList.remove('reveal'):
-  ball3.classList.remove('reveal'):
+    ball1.classList.remove('reveal');
+    ball2.classList.remove('reveal');
+    ball3.classList.remove('reveal');
 }
 
-
+function displayResults() {
+    winsSpan.textContent = wins;
+    totalSpan.textContent = total;
+    lossesSpan.textContent = total - wins;
+}
 
 // set event listeners 
 button1.addEventListener('click', () => {
   // get user input
+    total++;
+    resetBalls();
   // use user input to update state 
+    const ballPlace = Math.ceil(Math.random() * 3);
+    if (ballPlace ===1) {
+        ball1.classList.add('reveal');
+    } else if (ballPlace === 2) {
+      ball2.classList.add('reveal');
+    }
+
   // update DOM to reflect the new state
-  
+  displayResults();
 });
