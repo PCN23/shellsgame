2fa70a0239183b4dbfb70f6934c40a02ec1fcc8a
Pedro Chavez Nolasco <pedro.chn01@gmail.com>
2022-05-06 11:57:21 -0700

    starting shell game over

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 // import functions and grab DOM elements
+const ball1 = document.getElementById('ball1');
+const ball2 = document.getElementById('ball2');
+const ball3 = document.getElementById('ball3');
+
 
 // let state
+let wins = 0;
 
 // set event listeners 
   // get user input
